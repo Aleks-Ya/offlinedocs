14967940a1dcf1a2b690cf16668b7f301b4218c9
Aleksei Iablokov <aleksei_iablokov@epam.com>
2017-04-05 18:20:21 +0300

    add todo

--- src/test/java/ru/yaal/offlinedocs/impl/storage/UserDirArtifactStorageTest.java
+++ src/test/java/ru/yaal/offlinedocs/impl/storage/UserDirArtifactStorageTest.java
@@ -14,6 +14,7 @@ import static org.junit.Assert.assertThat;
 
 /**
  * TODO download file to temp dir and then copy to artifact storage
+ * TODO unpack file to temp dir and then copy to outlet
  *
  * @author Yablokov Aleksey
  */
